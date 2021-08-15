7adc5068d463d17650ad839fff28d877f8a3826d
Dawon00 <71630722+Dawon00@users.noreply.github.com>
2021-08-15 21:54:41 +0900

    Create 최소공배수(함수).cpp

---
+++ 1-2/최소공배수(함수).cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int func()
+{
+	int a, b,temp,lcm;
+	scanf("%d %d", &a, &b);
+
+	int gcd, temp_a, temp_b;
+	if(a>=b){
+		temp_a = a;
+		temp_b = b;
+	}
+	else{
+		temp_a = b;
+		temp_b = a;
+	}
+	//temp_a = a;
+	//temp_b = b;
+	
+	while(temp_a % temp_b != 0) 
+	{
+		temp = temp_a % temp_b;
+		temp_a = temp_b;
+		temp_b = temp;
+	}
+	//temp_b가 최대공약수
+	
+	gcd = temp_b;
+	lcm = (a*b) / gcd;
+	printf("%d", lcm); //lcm변수는 최소공배수
+
+}
+
+int main()
+{
+
+	func();
+	
+
+	return 0;
+}
